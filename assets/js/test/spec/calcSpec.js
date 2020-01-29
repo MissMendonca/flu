bafcf54dc619bc4a758bf0d37fc9047be0460c04
Ubuntu <ubuntu@ip-172-31-80-230.ec2.internal>
2020-01-29 09:14:01 +0000

    test user input with jasmine framework

---
+++ assets/js/test/spec/calcSpec.js
@@ -0,0 +1,7 @@
+describe("PostcodeValidator", function(){
+    describe("input tests", function() {
+        it("should return true", function(){
+            expect(text(" ")).toBe(false);
+        });
+    });
+});
