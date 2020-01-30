5c97c8f3afa1e3ce3ff605460d264751f62323dc
Ubuntu <ubuntu@ip-172-31-80-230.ec2.internal>
2020-01-30 23:10:13 +0000

    Created PostcodeValidator using postcodes.io api

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,35 @@ function text(userInput){
     //Regex expression to accept only strings containing a mix of numbers and letters in a range from 5 to 7 characters
     var letterNumber =/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]{5,7})$/;
     if (letterNumber.test(userInput.value)){
-        return $("#results").html(` Pharmacies near `+ userInput.value);
+        return postcodeValidator(userInput.value.toUpperCase());
     }
-    return $("#results").html(`Invalid Postcode`);
+    else{
+        return $("#results").html(`Invalid Postcode`);
+    }
 }
+
+function postcodeValidator(info){
+    var url = "https://api.postcodes.io/postcodes/"+info;
+    fetch(url)
+    .then(function(response) {
+        if (response.status !== 200) {
+            return $("#results").html(`Invalid Postcode`);
+        }
+        response.json()
+        .then(function(data) {
+            var latitude = data.result.latitude;
+            var longitude = data.result.longitude;
+            var locationData = [latitude, longitude];
+            console.log(locationData);
+        })
+        .catch(function(err) {
+            console.log('Error:', err);
+        })
+    });
+}
+
+
+
 
 $(document).ready(function(){
     
