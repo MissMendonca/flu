c70680f43312b1409fd9b0277b99ee96025089cd
Ubuntu <ubuntu@ip-172-31-80-230.ec2.internal>
2020-01-30 21:17:49 +0000

    Changed test function, changed form index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,12 @@
 function text(userInput){
     //Regex expression to accept only strings containing a mix of numbers and letters in a range from 5 to 7 characters
     var letterNumber =/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]{5,7})$/;
-    if (letterNumber.test(userInput)){
-        return true;
+    if (letterNumber.test(userInput.value)){
+        return $("#results").html(` Pharmacies near `+ userInput.value);
     }
-    return false;
+    return $("#results").html(`Invalid Postcode`);
 }
+
+$(document).ready(function(){
+    
+});
