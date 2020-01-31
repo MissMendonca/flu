e8d5c38d70d8d65bfa4ab2ef3ce5edb7e6b848f5
Ubuntu <ubuntu@ip-172-31-80-230.ec2.internal>
2020-01-31 15:41:25 +0000

    created fetchPharmaciesInfo and postcodeValidator

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,21 +14,42 @@ function postcodeValidator(info){
     fetch(url)
     .then(function(response) {
         if (response.status !== 200) {
-            return $("#results").html(`Invalid Postcode`);
+            return $("#results").html('Invalid Postcode');
         }
         response.json()
         .then(function(data) {
             var latitude = data.result.latitude;
             var longitude = data.result.longitude;
             var locationData = [latitude, longitude];
-            console.log(locationData);
+            fetchPharmaciesInfo(locationData);
         })
         .catch(function(err) {
             console.log('Error:', err);
-        })
+        });
     });
 }
 
+function fetchPharmaciesInfo(postArray){
+    var url = `https://api.foursquare.com/v2/venues/explore?client_id=LAXJFATFWWAWOJR4MNAMTXYPHPBJFCZLXXLEF0WHNHIYRBDZ&client_secret=5M4HU3K12I2FXB1C0T43MUZETLL14DCUEEPW55ZBMM5DFRSY&v=20180323
+        &intent=checkin&limit=15&ll=${postArray[0]},${postArray[1]}&query=pharmacy`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        $("#results").html(data.response.groups[0].items.map(function(item){
+        return `<div class="container"> 
+                    <h4>${item.venue.name}</h4>
+                    <p>${item.venue.location.formattedAddress[0]}</p>
+                    <p>${item.venue.location.formattedAddress[1]}</p>
+                    <p>${item.venue.location.formattedAddress[2]}</p>
+                    <p>${item.venue.location.formattedAddress[3]}</p>
+                    <p>${item.venue.location.formattedAddress[4]}</p>
+                </div>`;
+        }));
+    })
+    .catch(function(err) {
+        console.log('Error:', err);
+    });
+}
 
 
 
